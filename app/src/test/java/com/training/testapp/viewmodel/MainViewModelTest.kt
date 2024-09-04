67b815fc1e9bcd274720d1f545bebab483288baf
varunchandran333 <varunchandran333@gmail.com>
2024-09-04 08:47:19 -0500

    Test cases added

--- app/src/test/java/com/training/testapp/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/training/testapp/viewmodel/MainViewModelTest.kt
@@ -7,8 +7,14 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.test.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -48,8 +54,57 @@ class MainViewModelTest {
             assertEquals(expectedResult, awaitItem())
 
             // Ensure no more emissions
-            awaitComplete()
+            //awaitComplete() //State flow never completes
         }
+    }
+
+    @Test
+    fun `test userData flow emits loading and then success v1`() = runTest {
+        val mockData = listOf(User(id = 1, title = "John Doe"))
+        val expectedResult = NetworkResult.Success(mockData)
+        coEvery { repository.getApiData() } returns flow {
+            emit(NetworkResult.Loading)
+            emit(expectedResult)
+        }
+
+        viewModel = MainViewModel(repository)
+
+        viewModel.userData.test {
+            var emittedItem = awaitItem()
+            assertEquals(NetworkResult.Loading, emittedItem)
+            emittedItem = awaitItem()
+            assertEquals(mockData, (emittedItem as? NetworkResult.Success)?.data)
+        }
+    }
+
+    @Test
+    fun `test userData flow emits loading and then success v2`() = runTest {
+        val mockData = listOf(User(id = 1, title = "John Doe"))
+        val expectedResult = NetworkResult.Success(mockData)
+        coEvery { repository.getApiData() } returns flow {
+            emit(NetworkResult.Loading)
+            delay(1000L)
+            emit(NetworkResult.Exception(Throwable("Test simulate error")))
+            delay(2000L)
+            emit(expectedResult)
+        }
+
+        viewModel = MainViewModel(repository)
+
+        val list = mutableListOf<NetworkResult<List<User>>>()
+
+        val job = launch {
+            viewModel.userData.collect {
+                list.add(it)
+            }
+        }
+
+        advanceUntilIdle()
+
+        assertEquals(NetworkResult.Loading, (list.first() as? NetworkResult.Loading))
+        assertEquals("Test simulate error", (list[1] as? NetworkResult.Exception)?.e?.message)
+        assertEquals(mockData, (list.last() as? NetworkResult.Success)?.data)
+        job.cancel()
     }
 
     @After
